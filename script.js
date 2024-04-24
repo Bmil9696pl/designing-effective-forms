30ca6b625d3d13660459f7c02710fc20138ca093
bmil96 <bmil9696@gmail.com>
2024-04-24 15:27:50 +0200

    effective form

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const countryInput = document.getElementById('country');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
+const countryName = document.getElementById('countryName')
 
 function handleClick() {
     clickCount++;
@@ -17,6 +18,11 @@ async function fetchAndFillCountries() {
             throw new Error('Błąd pobierania danych');
         }
         const data = await response.json();
+        data.sort((a,b) => {
+            const aName = a.name.common;
+            const bName = b.name.common;
+            return (aName < bName) ? -1 : (aName > bName) ? 1 : 0;
+          });
         const countries = data.map(country => country.name.common);
         countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
     } catch (error) {
@@ -24,20 +30,21 @@ async function fetchAndFillCountries() {
     }
 }
 
-function getCountryByIP() {
+async function getCountryByIP() {
     fetch('https://get.geojs.io/v1/ip/geo.json')
         .then(response => response.json())
         .then(data => {
             const country = data.country;
-            document.getElementById('countryName').textContent = country;
+            countryInput.value = country;
+            getCountryCode(country)
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
         });
 }
 
-function getCountryCode() {
-    const countryName = document.getElementById('countryName').textContent;
+async function getCountryCode(countryName) {
+    console.log(countryName)
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
     fetch(apiUrl)
@@ -49,6 +56,8 @@ function getCountryCode() {
     })
     .then(data => {        
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
+        console.log(countryCode)
+        document.getElementById('countryCallingNumber').value = countryCode
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
@@ -61,4 +70,5 @@ function getCountryCode() {
     document.addEventListener('click', handleClick);
     
     fetchAndFillCountries();
+    getCountryByIP();
 })()
